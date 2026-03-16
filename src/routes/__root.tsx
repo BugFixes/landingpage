de30794c2a213edad88c29685ccb710f4989d3ac
Keloran <keloran@develbox.info>
2026-03-16 23:17:24 +0000

    added logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,6 +24,11 @@ export const Route = createRootRoute({
 		],
 		links: [
 			{
+				rel: "icon",
+				href: "/favicon.png",
+				type: "image/png",
+			},
+			{
 				rel: "stylesheet",
 				href: appCss,
 			},
